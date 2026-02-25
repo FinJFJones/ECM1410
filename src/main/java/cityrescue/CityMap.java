fc18abc4474f878ff8fb46bc1862b3a92d43e04f
Finley Jones <finley@finleyjones.co.uk>
2026-02-25 16:14:54 +0000

    CityMap structure

--- src/main/java/cityrescue/CityMap.java
+++ src/main/java/cityrescue/CityMap.java
@@ -5,5 +5,19 @@ package cityrescue;
 // Legal Move
 
 public class CityMap {
+    int[] gridSize;
+    int w;
+    int h;
+    boolean[][] blocked;
 
+    public CityMap(int[] gridSize) {
+        this.gridSize = gridSize;
+        this.w = this.gridSize[0];
+        this.h = this.gridSize[1];
+        this.blocked = new boolean[this.w][this.h];
+    }
+
+    public boolean isLegalMove() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
