3dfba0eb8bd2053809fb0c292363ef1b67cc569b
TJFRexeter <tjfr201@exeter.ac.uk>
2026-02-25 18:12:53 +0000

    station units setup

--- src/main/java/cityrescue/Station.java
+++ src/main/java/cityrescue/Station.java
@@ -3,12 +3,18 @@ package cityrescue;
 public class Station {
     String name;
     int Id;
-    Unit[] units;
     int[] loc;
+
+    Unit[] units;
+    int maxUnits;
+    int numUnits;
+
     public Station(String name, int Id,int[] loc){
         this.name = name;
         this.Id = Id;
         this.loc = loc;
-        this.units = new Unit[200]; //WORST CASE 200 UNITS IN 1 STATION
+        this.maxUnits = 200; //worst case has 200 units (all of the possible units)
+        this.units = new Unit[this.maxUnits];
+        this.numUnits = 0;
     }
 }
