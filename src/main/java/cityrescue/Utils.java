90cf4e3e0a7d027e3c4fab94d8ccd11d491ea152
TJFRexeter <tjfr201@exeter.ac.uk>
2026-02-25 19:03:57 +0000

    linear search

--- src/main/java/cityrescue/Utils.java
+++ src/main/java/cityrescue/Utils.java
@@ -2,6 +2,11 @@ package cityrescue;
 
 public class Utils {
     public boolean linearSearch(int[] arr, int x) {
-
+        for (int elem : arr) {
+            if (elem == x){
+                return true;
+            }
+        }
+        return false;
     }
 }
